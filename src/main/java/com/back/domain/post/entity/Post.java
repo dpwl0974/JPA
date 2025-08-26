e2cc3da3f4ff7c8085acec600b6147d829069c36
dpwl0974 <dpwl0974@gmail.com>
2025-08-26 11:14:50 +0900

    refactor: 베이스 엔터티 생성

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,15 +1,14 @@
 package com.back.domain.post.entity;
 
-import jakarta.persistence.*;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,20 +16,11 @@ import java.time.LocalDateTime;
 @ToString
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private int id;
+public class Post extends BaseEntity {
     private String title; // final -> 변수의 상수화 -> 명시적으로 값 넣어야 ⭕️
 
     @Column(columnDefinition = "TEXT")
     private String content;
-
-    @CreatedDate // 알아서 기계가
-    private LocalDateTime createDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
 
     // 매개변수 ❌-> 기본 생성자
     public Post(String title, String content) {
