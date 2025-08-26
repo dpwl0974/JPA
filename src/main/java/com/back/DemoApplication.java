5b36c49910aeea27a557518f9f0763a317514282
dpwl0974 <dpwl0974@gmail.com>
2025-08-26 10:46:27 +0900

    fix: 잘못된 어노테이션 수정

--- src/main/java/com/back/DemoApplication.java
+++ src/main/java/com/back/DemoApplication.java
@@ -2,10 +2,10 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableJpaAuditing
 public class DemoApplication {
 
 	public static void main(String[] args) {
